52587b6790c3548dfaaf9f13477ed8796dbee0d1
Munjal Patel <munjalpatel@gmail.com>
2015-02-08 17:47:00 -0800

    Now requiring subdomain to be passed into options

--- src/chargify.js
+++ src/chargify.js
@@ -1,5 +1,6 @@
 function Chargify(options) {
   if (!options) throw new Error('No options specified');
+  if (!options.subdomain) throw new Error('No subdomain specified');
 }
 
 module.exports = Chargify;
--- src/test/chargify.js
+++ src/test/chargify.js
@@ -5,4 +5,8 @@ describe('Chargify', function () {
   it('should throw an error if no options specified', function () {
     expect(chargify.bind(chargify)).to.throw(/No options specified/);
   });
+
+  it('should throw an error if no subdomain is specified', function () {
+    expect(chargify.bind(chargify, {})).to.throw(/No subdomain specified/);
+  });
 });
