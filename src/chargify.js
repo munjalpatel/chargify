9b8032d2e2c5bcf022d8f639483a66b939ed724f
Munjal Patel <munjalpatel@gmail.com>
2015-02-13 14:00:58 -0800

    Restricting method to be: GET, POST, PUT or DELETE

--- src/chargify.js
+++ src/chargify.js
@@ -4,9 +4,20 @@ function Chargify(options) {
   if (!options.api_key) throw new Error('No API key specified');
 }
 
-Chargify.prototype.request = function (resource, method) {
-  if (!resource) throw new Error('No resource specified');
-  if (!method) throw new Error('No method specified');
+Chargify.prototype.request = function (options) {
+  if (!options) throw new Error('No options specified');
+  if (!options.resource) throw new Error('No resource specified');
+  if (!options.method) throw new Error('No method specified');
+
+  switch (options.method) {
+  case 'GET':
+  case 'POST':
+  case 'PUT':
+  case 'DELETE':
+    break;
+  default:
+    throw new Error('Invalid method. Supported methods are: GET, POST, PUT, DELETE');
+  }
 };
 
 module.exports = Chargify;
--- src/test/request.js
+++ src/test/request.js
@@ -8,9 +8,14 @@ var options = {
 
 describe('Chargify', function () {
   describe('request()', function () {
+    it('should throw an error if no options are specified', function () {
+      var chargify = new Chargify(options);
+      expect(chargify.request.bind(chargify)).to.throw(/No options specified/);
+    });
+
     it('should throw an error if no resource is specified', function () {
       var chargify = new Chargify(options);
-      expect(chargify.request.bind(chargify)).to.throw(/No resource specified/);
+      expect(chargify.request.bind(chargify, {})).to.throw(/No resource specified/);
     });
 
     it('should throw an error if no method is specified', function () {
@@ -18,6 +23,16 @@ describe('Chargify', function () {
       expect(chargify.request.bind(chargify, {
         resource: 'test'
       })).to.throw(/No method specified/);
+    });
+
+    describe('method', function () {
+      it('should be GET, POST, PUT or DELETE', function () {
+        var chargify = new Chargify(options);
+        expect(chargify.request.bind(chargify, {
+          resource: 'test',
+          method: 'test'
+        })).to.throw(/Invalid method. Supported methods are: GET, POST, PUT, DELETE/);
+      });
     });
   });
 });
